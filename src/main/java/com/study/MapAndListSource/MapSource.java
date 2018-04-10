25353a87b7e4880bfbfd3f11b357a4d63fe03020
mzq <www4836@gmail.com>
2018-04-10 16:38:48 +0800

    commit

--- src/main/java/com/study/MapAndListSource/MapSource.java
+++ src/main/java/com/study/MapAndListSource/MapSource.java
@@ -12,14 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MapSource {
     public static void main(String[] args) {
         Map hashMap = new HashMap();
-        hashAppendTest test = new hashAppendTest(hashMap);
-        Thread t1 = new Thread(test,"thread1");
-        Thread t2 = new Thread(test,"thread2");
-        t1.start();
-        t2.start();
 
-        System.out.println(hashMap.toString());
-        //  1.extends AbstractMap
 
 
         Map Hashtable = new Hashtable();
@@ -37,24 +30,3 @@ public class MapSource {
     }
 }
 
-class hashAppendTest implements Runnable {
-
-    Map hashMap;
-
-    hashAppendTest(Map hashMap){
-        this.hashMap = hashMap;
-    }
-
-    @Override
-    public void run() {
-        this.hashMap  = hashAppend(hashMap);
-        System.out.println(this.hashMap);
-    }
-
-    Map hashAppend(Map hashMap) {
-        for (int i = 0; i < 100; i++) {
-            hashMap.put(i, i + "-");
-        }
-        return hashMap;
-    }
-}
